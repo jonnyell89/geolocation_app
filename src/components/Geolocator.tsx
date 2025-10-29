2926b94c36f6b33ffe71153cceb4af6bb1685706
jonnyell89 <jonnyell89@gmail.com>
2025-10-29 21:14:22 +0000

    index.css styling added

--- src/components/Geolocator.tsx
+++ src/components/Geolocator.tsx
@@ -13,10 +13,12 @@ function Geolocator() {
     const [error, setError] = useState<string | null>(null);
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        
         setCoords({...coords, [event.target.name]: event.target.value});
     }
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        
         event.preventDefault();
 
         const lat = parseFloat(coords.latitude);
@@ -29,6 +31,7 @@ function Geolocator() {
         }
 
         try {
+
             setStatus("loading");
             setError(null);
             
@@ -53,6 +56,7 @@ function Geolocator() {
             setStatus("success");
 
         } catch(err: any) {
+
             setError(err.message || "Something went wrong when fetching geolocation data.");
             setStatus("error");
         }
@@ -61,10 +65,14 @@ function Geolocator() {
     return (
         <>
             <form onSubmit={handleSubmit}>
-                <label>Latitude: </label>
-                <input type="text" name="latitude" value={coords.latitude} onChange={handleChange} /><br />
-                <label>Longitude: </label>
-                <input type="text" name="longitude" value={coords.longitude} onChange={handleChange} /><br />
+                <div className="form-group">
+                    <label htmlFor="latitude">Latitude</label>
+                    <input type="text" id="latitude" name="latitude" value={coords.latitude} onChange={handleChange} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="longitude">Longitude</label>
+                    <input type="text" id="longitude" name="longitude" value={coords.longitude} onChange={handleChange} />
+                </div>
                 <button type="submit">Submit</button>
             </form>
 
